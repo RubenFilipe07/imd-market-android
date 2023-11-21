8fa089845ce9b6f99e368dac8736992be8533ca8
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-11-21 14:29:55 -0300

    feat: add sqlite database

--- app/src/main/java/com/rubenfilipe07/imd_market_android/MainActivity.java
+++ app/src/main/java/com/rubenfilipe07/imd_market_android/MainActivity.java
@@ -2,13 +2,28 @@ package com.rubenfilipe07.imd_market_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private SQLiteDatabase bancoDados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        criarBancoDados();
+    }
+
+    public void criarBancoDados() {
+        try {
+            bancoDados = openOrCreateDatabase("produtos", MODE_PRIVATE, null);
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS produtos (id INTEGER PRIMARY KEY AUTOINCREMENT, codigo_produto VARCHAR, nome_produto VARCHAR, descricao_produto VARCHAR, estoque INTEGER)");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
