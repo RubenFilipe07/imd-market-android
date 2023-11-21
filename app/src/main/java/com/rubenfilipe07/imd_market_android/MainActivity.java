a1cc8f4438b8ab6d41738a5ebafe965d2b92820c
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-11-21 16:01:44 -0300

    feat: add sharedPreferences login

--- app/src/main/java/com/rubenfilipe07/imd_market_android/MainActivity.java
+++ app/src/main/java/com/rubenfilipe07/imd_market_android/MainActivity.java
@@ -2,19 +2,23 @@ package com.rubenfilipe07.imd_market_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-
-import java.util.ArrayList;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase bancoDados;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inicializaSharedPreferences();
         criarBancoDados();
+
     }
 
     public void criarBancoDados() {
@@ -24,6 +28,28 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void login(View view) {
+        EditText inputLogin = findViewById(R.id.inputLogin);
+        EditText inputSenha = findViewById(R.id.inputSenha);
+        String login = inputLogin.getText().toString();
+        String senha = inputSenha.getText().toString();
+        SharedPreferences sharedPreferences = getSharedPreferences("login", MODE_PRIVATE);
+        String loginShared = sharedPreferences.getString("login", "");
+        String senhaShared = sharedPreferences.getString("senha", "");
+        if (login.equals(loginShared) && senha.equals(senhaShared)) {
+            setContentView(R.layout.activity_menu);
+        }
+    }
+
+
+    public void inicializaSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("login", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("login", "admin");
+        editor.putString("senha", "admin");
+        editor.commit();
     }
 }
 
